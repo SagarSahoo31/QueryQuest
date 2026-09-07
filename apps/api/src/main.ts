6b76e2fee91901857ef9e03bf0f78d921c37ceed
ForBeen <spookymicrosoftuser@gmail.com>
2026-09-08 02:37:44 +0530

    fix(api): resolve server type error

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,8 +5,7 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 
-let server: ReturnType<Awaited<ReturnType<typeof createApp>>['getHttpAdapter']['getInstance']>;
-
+let server: any;
 async function createApp() {
   const app = await NestFactory.create(AppModule);
 
